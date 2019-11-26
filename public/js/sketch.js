6b3fd6e7e5ce6deb31b36b32bdd804b432946499
milzar <realmilanxavier@gmail.com>
2019-11-26 20:44:35 +0530

    Add. Save and download sketch in browser as jpg

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -3,77 +3,85 @@ var c;
 
 // Prevent scroll lock on iOS
 // SOURCE: https://www.npmjs.com/package/body-scroll-lock
-const bodyScrollLock = require('body-scroll-lock');
-const disableBodyScroll = bodyScrollLock.disableBodyScroll;
-const enableBodyScroll = bodyScrollLock.enableBodyScroll;
-const targetElement = document.querySelector("#disableScroll")
+bodyScrollLock = require('body-scroll-lock');
+disableBodyScroll = bodyScrollLock.disableBodyScroll;
+enableBodyScroll = bodyScrollLock.enableBodyScroll;
+targetElement = document.querySelector("#disableScroll");
+
+var cnv;
+
+function submitSketch() {
+    console.log("Saving Sketch");
+    saveCanvas(cnv, 'faces', 'jpg');
+}
+
 
 function setup() {
-  var cnv = createCanvas(windowWidth/2 + 220, windowHeight/2 + 220);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-  // canvas.parent('sketch-holder');
+    cnv = createCanvas(windowWidth / 2 + 220, windowHeight / 2 + 220);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    cnv.parent('sketch-holder');
+    // canvas.parent('sketch-holder');
 
-  // createP();
+    // createP();
 
-  slider = createSlider(1, 20, 10);
-  slider.position(x,115);
+    slider = createSlider(1, 20, 10);
+    slider.position(x, 115);
 
-  eraser = createCheckbox(' Eraser', false);
-  eraser.position(windowWidth/2 - 35, 110);
+    eraser = createCheckbox(' Eraser', false);
+    eraser.position(windowWidth / 2 - 35, 110);
 
-  clear = createButton("Clear Canvas");
-  clear.mousePressed(changeBG);
-  clear.position(600,106);
+    clear = createButton("Clear Canvas");
+    clear.mousePressed(changeBG);
+    clear.position(600, 106);
 
-  c = color(0, 0, 0);
-  background(255);
-  colorMode(RGB)
+    c = color(0, 0, 0);
+    background(255);
+    colorMode(RGB)
 
-  // saveCanvas(cnv, './outputSketch/faces', 'jpg');
 }
 
 function draw() {
-  radius = slider.value();
+    radius = slider.value();
 }
 
 disableBodyScroll(targetElement);
 
 function mouseClicked() {
-  if (mouseX > 400) {
-    c = get(mouseX, mouseY);
-    eraser.checked(false);
-  } else  {
-    stampRectangle(c);
-  }
+    if (mouseX > 400) {
+        c = get(mouseX, mouseY);
+        eraser.checked(false);
+    } else {
+        stampRectangle(c);
+    }
 }
 
 function mouseDragged() {
-  if (eraser.checked()){
-    stroke(255);
-  } else  {
-    stroke(c)
-  }
-  if (mouseX < windowHeight) {
-    strokeWeight(slider.value());
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+    if (eraser.checked()) {
+        stroke(255);
+    } else {
+        stroke(c)
+    }
+    if (mouseX < windowHeight) {
+        strokeWeight(slider.value());
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
 }
 
 function changeBG() {
-  // background(255);
-  // setup();
-  window.open("sketch.html","_self");
+    // background(255);
+    // setup();
+    window.open("sketch.html", "_self");
 }
 
 
-function stampRectangle(c)  {
-  fill(c)
-  noStroke()
-  rect(mouseX,mouseY,slider.value(),slider.value())
+function stampRectangle(c) {
+    fill(c)
+    noStroke()
+    rect(mouseX, mouseY, slider.value(), slider.value())
 }
 
 function eraserSwitch() {
- // 
+    //
 }
