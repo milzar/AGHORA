9dfe16fb22c44128f1168869b4a7dda9003b7eb9
milzar <realmilanxavier@gmail.com>
2019-12-01 12:35:00 +0530

    Fix. Sketches are saved to images folder

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.post('/submit-sketch', function (req, res, next) {
   var base64Sketch = req.body.data;
 
-  require("fs").writeFile("./public/images/sketch.jpg", base64Sketch, 'base64', function (err) {
+  require("fs").writeFile("./public/outputSketch/sketch.jpg", base64Sketch, 'base64', function (err) {
     console.log(err);
   });
 });
